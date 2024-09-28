381b5ea3f88e935488599105faee6b778f1dcedb
JavaGiao-Giao <702630631@qq.com>
2024-09-28 15:20:45 +0800

    Update GitTest.java
    在GitHub上修改代码

--- springboot-02/src/main/java/com/lgqlgq/springboot/controller/GitTest.java
+++ springboot-02/src/main/java/com/lgqlgq/springboot/controller/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("第二次代码提交测试代码");
         System.out.println("第三次修改测试");
         System.out.println("第四次提交");
+        System.out.println("在GitHub上修改代码，在idea进行拉取行为，观察状态");
     }
 }
