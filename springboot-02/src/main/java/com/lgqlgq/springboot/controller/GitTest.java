4cc6c87d72b3d5f2557e6ec6571b39f107397c43
lgq <lgq@qq.com>
2024-09-28 14:22:10 +0800

    第三次提交尝试，修改代码GitTest于springmvc02

--- springboot-02/src/main/java/com/lgqlgq/springboot/controller/GitTest.java
+++ springboot-02/src/main/java/com/lgqlgq/springboot/controller/GitTest.java
@@ -3,5 +3,6 @@ package com.lgqlgq.springboot.controller;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("第二次代码提交测试代码");
+        System.out.println("第三次修改测试");
     }
 }
